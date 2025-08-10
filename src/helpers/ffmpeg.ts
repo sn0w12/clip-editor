29deef86cfb7673dc7489a2ee9c5a01237cca366
Lucas Sköld <lucasskold49@gmail.com>
2025-08-10 02:30:53 +0200

    Add ffprobe-static

--- src/helpers/ffmpeg.ts
+++ src/helpers/ffmpeg.ts
@@ -1,5 +1,9 @@
 import ffmpeg from "fluent-ffmpeg";
 import pathToFfmpeg from "ffmpeg-static";
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const ffprobe = require("ffprobe-static");
+
 ffmpeg.setFfmpegPath(pathToFfmpeg || "");
+ffmpeg.setFfprobePath(ffprobe.path || "");
 
 export default ffmpeg;

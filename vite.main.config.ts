99266f69034c1cac5b6f5b4910db79d503ac2380
Lucas Sköld <lucasskold49@gmail.com>
2025-08-11 04:08:21 +0200

    Remove unused rollupOptions

--- vite.main.config.ts
+++ vite.main.config.ts
@@ -4,15 +4,6 @@ import path from "path";
 // https://vitejs.dev/config
 export default defineConfig({
     build: {
-        rollupOptions: {
-            external: [
-                "ffmpeg-static",
-                "ffprobe-static",
-                "update-electron-app",
-                "electron-squirrel-startup",
-                "sharp",
-            ],
-        },
         chunkSizeWarningLimit: 1600,
     },
     resolve: {
